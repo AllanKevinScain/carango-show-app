21687883c0657faa8e4dc3228476d476ee55c2ea
Manuela Kleinkauf de Oliveira <manuelakleinkauf.o@gmail.com>
2025-09-30 16:17:23 -0300

    fix: paginacao dos pedidos

--- src/hooks/use-order/index.tsx
+++ src/hooks/use-order/index.tsx
@@ -1,5 +1,4 @@
 import { api } from "@/api";
-import { useQuery } from "@tanstack/react-query";
 import * as yup from "yup";
 
 export const orderSchema = yup.object().shape({
@@ -10,16 +9,29 @@ export const orderSchema = yup.object().shape({
 
 export type OrderType = yup.InferType<typeof orderSchema>;
 
+export type ListOrderReturnType = {
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+  data: OrderType[];
+};
+
+interface ListOrderInterface {
+  page?: number;
+  limit?: number;
+}
+
 export function useOrder() {
-  async function listOrders() {
-    const response = await api.get("/order");
-    return response.data as OrderType[];
+  async function listOrders(props?: ListOrderInterface) {
+    if (props) {
+      const { page = 1, limit = 50 } = props;
+      const response = await api.get(`/order?page=${page}&limit=${limit}`);
+      return response.data;
+    }
   }
 
-  const query = useQuery<OrderType[]>({
-    queryKey: ["order"],
-    queryFn: listOrders,
-  });
-
-  return query;
+  return {
+    listOrders,
+  };
 }
