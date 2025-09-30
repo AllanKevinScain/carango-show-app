6f9019961e32e7b5ae5b03f681e7a0c463684c83
allankevinscain <meuemail44allan@gmail.com>
2025-09-30 17:12:51 -0300

    chore: errors bond

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,36 +11,39 @@ import { LayoutDefault, LayoutLogin } from "./layouts";
 import { CartPage } from "./pages/cart";
 import { QueryProvider, SessionProvider } from "@/providers";
 import { OrderPage } from "./pages/order";
+import { ErrorBoundary } from "./pages/error";
 
 export const CustomRoutes = () => {
   return (
     <SessionProvider>
       <QueryProvider>
         <Routes>
-          <Route element={<LayoutLogin />}>
+          <Route element={<LayoutLogin />} ErrorBoundary={ErrorBoundary}>
             <Route index path="/" element={<LoginPage />} />
             <Route path="register" element={<RegisterPage />} />
           </Route>
 
-          <Route element={<LayoutDefault />}>
+          <Route element={<LayoutDefault />} ErrorBoundary={ErrorBoundary}>
             <Route index path="product" element={<ListPage />} />
             <Route path="product/:productId" element={<ProductPage />} />
             <Route path="cart" element={<CartPage />} />
             <Route
               path="congratulations"
-              element={<h1>Parabéns voce finalizou o pedido!</h1>}
+              element={<h1>Parabéns você finalizou o pedido!</h1>}
             />
           </Route>
 
-          <Route element={<LayoutDefault />}>
+          <Route element={<LayoutDefault />} ErrorBoundary={ErrorBoundary}>
             <Route index path="admin" element={<AdminPage />} />
             <Route path="order" element={<OrderPage />} />
           </Route>
 
-          <Route element={<LayoutDefault />}>
+          <Route element={<LayoutDefault />} ErrorBoundary={ErrorBoundary}>
             <Route index path="profile/:id" element={<ProfilePage />} />
             <Route path="profile/order" element={<h1>Profile order</h1>} />
           </Route>
+
+          <Route path="*" element={<h1>Essa página não existe</h1>} />
         </Routes>
       </QueryProvider>
     </SessionProvider>
