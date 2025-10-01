7bc2daa81f56efbd6556d00c3efd53693f7f098c
Manuela Kleinkauf de Oliveira <manuelakleinkauf.o@gmail.com>
2025-10-01 19:48:14 -0300

    fix: removendo pesquisa

--- src/routes/pages/admin/index.tsx
+++ src/routes/pages/admin/index.tsx
@@ -1,17 +1,17 @@
-import { Button, Container, TextField } from "@/components";
+import { Button, Container } from "@/components";
 import { twMerge } from "tailwind-merge";
 import { CreateModal, List } from "./components";
 import { useToggle } from "@/hooks";
-import { useForm } from "react-hook-form";
+// import { useForm } from "react-hook-form";
 
 export function AdminPage() {
   const createModal = useToggle();
 
-  const { control } = useForm({
-    defaultValues: {
-      search: "",
-    },
-  });
+  // const { control } = useForm({
+  //   defaultValues: {
+  //     search: "",
+  //   },
+  // });
 
   return (
     <>
@@ -33,10 +33,10 @@ export function AdminPage() {
             Novo
           </Button>
         </div>
-        <div className={twMerge("flex gap-[24px] items-center", "w-full")}>
+        {/* <div className={twMerge("flex gap-[24px] items-center", "w-full")}>
           <TextField id="search" placeholder="Pesquisar..." control={control} />
           <Button className="w-fit h-fit px-3 py-2">Pesquisar</Button>
-        </div>
+        </div> */}
 
         <List />
       </Container>
